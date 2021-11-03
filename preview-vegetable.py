b417a4453ad6922ff0ead01fa9ec7860df1c2a84
okome-git <n.tatemizo@gmail.com>
2021-11-03 22:56:42 +0900

    password

--- preview-vegetable.py
+++ preview-vegetable.py
@@ -13,11 +13,7 @@ st.set_page_config(
     page_icon='🍅',
 )
 
-"""
-# 🍅デジベジ
-## 現場最新画像閲覧システム
-____
-"""
+
 stores = ["aeon_rifu_1","aeon_rifu_2", "aeon_rifu_3", "aeon_rifu_4"]
 
 bucket_name = "vegi-upload-images"
@@ -29,6 +25,30 @@ s3 = boto3.client('s3',
                   aws_secret_access_key=st.secrets["AWS_SECRET_ACCESS_KEY"],
                   region_name='ap-northeast-1'
                   )
+
+def is_authenticated(password):
+    return password == st.secrets["PRE_PASSWORD"]
+
+def generate_login_block():
+    block1 = st.empty()
+    block2 = st.empty()
+
+    return block1, block2
+
+def clean_blocks(blocks):
+    for block in blocks:
+        block.empty()
+
+def login(blocks):
+    blocks[0].markdown("""
+            <style>
+                input {
+                    -webkit-text-security: disc;
+                }
+            </style>
+        """, unsafe_allow_html=True)
+
+    return blocks[1].text_input("パスワードを入力してください", help="わすれた場合はokomeに聞いてください", value="", type="password")
 
 def latest_image_path(store_name, bucket_name):
     dt = datetime.datetime.now()
@@ -57,8 +77,8 @@ def download_image(latest_image_dict):
         except:
             st.error(f"本日の{store_name}の画像をダウンロードできませんでした")
 
-def clear_upload_images():    
-    latest_image_dict = get_latest_image_paths(stores, bucket_name)
+def clear_upload_images(stores):    
+    latest_image_dict = get_latest_image_paths(stores, bucket_name=bucket_name)
     download_image(latest_image_dict)
 
 @st.cache
@@ -77,7 +97,24 @@ def get_device_list(bucket_name=bucket_name):
         device_list.append(o.get('Prefix').split("/")[0])
     return device_list
 
-def main(images, Pil_Images, stores):
+def main(images, Pil_Images):
+    """
+    # 🍅デジベジ
+    ## 現場最新画像閲覧システム
+    ____
+    """
+    stores = st.multiselect(
+    "端末を選ぶ",
+    device_list,
+    ["aeon_rifu_1","aeon_rifu_2", "aeon_rifu_3", "aeon_rifu_4"]
+    )
+    uppdate_button = st.button("最新画像に更新")
+    if uppdate_button:
+        state = st.empty()
+        state.write("最新の売り場画像に更新しています....")
+        clear_upload_images(stores)
+        state.success("更新完了")
+
     for i, image in enumerate(images):
         st.markdown("## 端末："+image.split("/")[-1].split(".")[0].split("_")[0])
         time = images[i].split("/")[-1].split(".")[0].split("_")[1:]
@@ -85,34 +122,18 @@ def main(images, Pil_Images, stores):
         st.image(Pil_Images[i], caption=images[i].split("/")[-1])
         st.markdown("___")
 
-uppdate_button = st.button("最新画像に更新")
+
 device_list = get_device_list()
-stores = st.multiselect(
-    "端末を選ぶ",
-    device_list,
-    ["aeon_rifu_1","aeon_rifu_2", "aeon_rifu_3", "aeon_rifu_4"]
-    )
 
-# with st.form(key='my_form'):
 
-#     stores = st.multiselect(
-#         "端末を選ぶ",
-#         device_list,
-#         ["aeon_rifu_1","aeon_rifu_2", "aeon_rifu_3", "aeon_rifu_4"]
-#         )
-# 	submit_button = st.form_submit_button('Submit')
+login_blocks = generate_login_block()
+password = login(login_blocks)
 
-# if submit_button:
-#     with open("./devices.json", "w") as f:
-#         devices_dict = {"device":device_list}
-#         json.dump(devices_dict, f)
+if is_authenticated(password):
+    clean_blocks(login_blocks)
+    images = glob("./latest_images/"+ "*.jpg")
+    Pil_Images = read_image(images)
+    main(images, Pil_Images)
 
-if uppdate_button:
-    state = st.empty()
-    state.write("最新の売り場画像に更新しています....")
-    clear_upload_images()
-    state.success("更新完了")
-
-images = glob("./latest_images/"+ "*.jpg")
-Pil_Images = read_image(images)
-main(images, Pil_Images, stores)
+elif password:
+    st.info("正しいパスワードを入力してください")
