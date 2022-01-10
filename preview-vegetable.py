bc3d04a336409b732680973b33aad9e22b701abf
okome <67691153+okome-git@users.noreply.github.com>
2022-01-10 15:54:56 +0900

    Update preview-vegetable.py

--- preview-vegetable.py
+++ preview-vegetable.py
@@ -23,8 +23,8 @@ bucket_name = "vege-upload-images"
 s3 = boto3.resource('s3')
 bucket = s3.Bucket('vege-upload-images')
 
-dynamodb = boto3.resource('dynamodb', )
-table = dynamodb.Table('dev-digivege-store-table', region_name='ap-northeast-1')
+dynamodb = boto3.resource('dynamodb', region_name='ap-northeast-1')
+table = dynamodb.Table('dev-digivege-store-table')
 
 s3 = boto3.client('s3',
                   aws_access_key_id=st.secrets["AWS_ACCESS_KEY_ID"],
